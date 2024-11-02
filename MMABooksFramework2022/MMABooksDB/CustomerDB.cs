3949fa32f8e90c90e5ee12b309fb10fd55d14155
Samuel Kjelde <kjeldes@my.lanecc.edu>
2024-11-02 05:46:02 -0700

    Looking good, just finish up comments with DB and classes, and should be good

--- MMABooksFramework2022/MMABooksDB/CustomerDB.cs
+++ MMABooksFramework2022/MMABooksDB/CustomerDB.cs
@@ -21,7 +21,21 @@ namespace MMABooksDB
 {
     public class CustomerDB : DBBase, IReadDB, IWriteDB
     {
+        // A default constructor that calls the base class
+        // class constructor using base() to initialize
+        // DBBase to have the have CustomerDB object be
+        // able to have the core functionality of being
+        // able to form a connection with a database and
+        // be able to perform stored procedures in the
+        // database.
         public CustomerDB() : base() { }
+
+        // A parameterized constructor with similar function
+        // to the default constructor, but now with it taking
+        // a DBConnection connection being used in the DBBase
+        // to form a connection to a database, and perform
+        // procedures that would require that, such as
+        // receiving and creating records.
         public CustomerDB(DBConnection cn) : base(cn) { }
 
         /*
@@ -64,14 +78,48 @@ namespace MMABooksDB
             }
         }
          */
+
+        // The Create Method is used to create a new customer
+        // record into a database such as MySQL, where is calls
+        // a stored procedure in the database called usp_CustomerCreate
+        // where it takes a IBaseProps object takes the fields of it
+        // and makes them into a compatible record that is then
+        // sent to the Customers table in the database with a
+        // automatically added CustomerID.
         public IBaseProps Create(IBaseProps p)
         {
             int rowsAffected = 0;
+
+            // This casts the IBaseProps as a 
+            // CustomerProps object, so you able
+            // to access the CustomerProps specific fields
+            // and methods for use in this method.
             CustomerProps props = (CustomerProps)p;
 
+            // This creates a DBCommand object hold the
+            // command that is going to be run alongside
+            // the parameters that it will follow.
             DBCommand command = new DBCommand();
+            // The stored procedure/command that is going
+            // to be runned. It's located in the database
             command.CommandText = "usp_CustomerCreate";
+            // Where then the command is marked as
+            // a stored procedure, so it's interpreted as so.
+            // Where it tells the database to check the
+            // CommandText, what was wrote a line above this one
+            // that we are using the stored procedure listed, which
+            // for this is usp_CustomerCreate.
             command.CommandType = CommandType.StoredProcedure;
+            // Where these are used to specify felids that are
+            // going to be passed to the stored procedure,
+            // this being what the field is called and the
+            // data type that is being used.
+            // ----------------------------
+            // With the custId one of this being an 
+            // output parameter which after the command runs
+            // it will be set to the a automatically created
+            // CustomerID that the databased created for the
+            // Customer record.
             command.Parameters.Add("custId", DBDbType.Int32);
             command.Parameters.Add("Name_p", DBDbType.VarChar);
             command.Parameters.Add("Address_p", DBDbType.VarChar);
@@ -79,7 +127,17 @@ namespace MMABooksDB
             command.Parameters.Add("State_p", DBDbType.VarChar);
             command.Parameters.Add("ZipCode_p", DBDbType.VarChar);
             //... there are more parameters here
+
+            // With this line being used to specify that the data is 
+            // is output only to the database, but will return a value
+            // such as the amount of rows in the database being
+            // affected.
             command.Parameters[0].Direction = ParameterDirection.Output;
+            // With these being used to assign values to the
+            // specified fields using the corresponding fields
+            // in the CustomerProps object to populate the
+            // the specific felids that are going to be passed
+            // to the stored procedure.
             command.Parameters["Name_p"].Value = props.Name;
             command.Parameters["Address_p"].Value = props.Address;
             command.Parameters["City_p"].Value = props.City;
@@ -89,14 +147,25 @@ namespace MMABooksDB
 
             try
             {
+                // Where then the created command is ran using
+                // RunNonQueryProcedure that will interact and
+                // performs it's specified procedure and set the
+                // rowsAffected with a int value if it worked.
                 rowsAffected = RunNonQueryProcedure(command);
                 if (rowsAffected == 1)
                 {
+                    // This is when the CustomerID field of the
+                    // CustomerProps object is set to the created
+                    // CustomerID that was made by the database.
                     props.CustomerID = (int)command.Parameters[0].Value;
                     props.ConcurrencyID = 1;
+                    // This it returns the CustomerProps with the
+                    // the created CustomerID and the ConcurrencyID
+                    // set to the corresponding felids.
                     return props;
                 }
                 else
+                    // If it failed then it will throw an Exception.
                     throw new Exception("Unable to insert record. " + props.ToString());
             }
             catch (Exception e)
@@ -106,11 +175,19 @@ namespace MMABooksDB
             }
             finally
             {
+                // Then it closes the connection if it's
+                // still open.
                 if (mConnection.State == ConnectionState.Open)
                     mConnection.Close();
             }
         }
 
+        // The Delete method that is used to delete a Customer
+        // record from a database where is calls a stored
+        // procedure in the database called usp_CustomerDelete
+        // where if the entered CustomerID and ConcurrencyID
+        // of a IBaseProps object matches those value in field
+        // of a given Customer record then it will be delated.
         public bool Delete(IBaseProps p)
         {
             CustomerProps props = (CustomerProps)p;
@@ -150,6 +227,12 @@ namespace MMABooksDB
             }
         }
 
+        // The Retrieve method is used to retrieve a specific
+        // customer record from a database where it calls
+        // the stored procedure usp_CustomerSelect, where
+        // based on the CustomerID that is entered it will
+        // return that specific Customer object that has
+        // that CustomerID.
         public IBaseProps Retrieve(object key)
         {
             DBDataReader data = null;
@@ -190,6 +273,13 @@ namespace MMABooksDB
             }
         }
 
+        // The RetrieveAll method is like the Retrieve method
+        // but it will return a list of all the current Customer
+        // records in the Customers Table turned into CustomerProps
+        // objects and then added to a list which will be returned
+        // to where this method was called. This is done by using
+        // the stored procedure usp_CustomerSelectAll from the
+        // database.
         public object RetrieveAll()
         {
             List<CustomerProps> list = new List<CustomerProps>();
@@ -224,6 +314,12 @@ namespace MMABooksDB
             }
         }
 
+        // The Update method is used to update the fields of
+        // a Customer record in the Customers Table of the
+        // database, using the stored procedure usp_CustomerUpdate
+        // where if the fields of record matches the inputted
+        // CustomerID and ConcurrencyID, then it will update
+        // the name of the Customer record.
         public bool Update(IBaseProps p)
         {
             int rowsAffected = 0;
--- MMABooksFramework2022/MMABooksTests/CustomerDBTests.cs
+++ MMABooksFramework2022/MMABooksTests/CustomerDBTests.cs
@@ -15,9 +15,16 @@ namespace MMABooksTests
     [TestFixture]
     public class CustomerDBTests
     {
+        // Creates a public CustomerDB class that will be used
+        // in all the tests to be able to use and test the
+        // CustomerDB methods.
         CustomerDB db;
-
         [SetUp]
+        // This will run before each test to clear the the CustomerDB
+        // object and to run the stored procedure usp_testingResetData
+        // to restart the tables that theses tests are going to be
+        // interacting with back to how they were before running a
+        // test.
         public void ResetData()
         {
             db = new CustomerDB();
@@ -28,6 +35,11 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This is used to test the Retrieve method from the
+        // CustomerDB class. Where it retrieves a record from
+        // the data, and they using AreEqual to see if the
+        // fields from the collected record match to what we
+        // expect them to be.
         public void TestRetrieve()
         {
             CustomerProps p = (CustomerProps)db.Retrieve(1);
@@ -36,6 +48,14 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This is used to test the RetrieveAll method from
+        // the CustomerDB class, where it runs the RetrieveAll
+        // method to set the list to all the current records
+        // in the Customer table in the database. Using
+        // AreEqual to see if it was able to grab all the
+        // current record by seeing if the grabbed amount
+        // matches the number of records there are
+        // supposed to be.
         public void TestRetrieveAll()
         {
             List<CustomerProps> list = (List<CustomerProps>)db.RetrieveAll();
@@ -43,6 +63,12 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This is used to test the Delete method from
+        // the CustomerDB class. Where it first uses the Retrieve
+        // method to grab a already existing record, then using
+        // the Delete method, then using Retrieve method again on the
+        // same record checking to see if it throws an error
+        // since the record should not exist anymore..
         public void TestDelete()
         {
             CustomerProps p = (CustomerProps)db.Retrieve(1);
@@ -51,6 +77,9 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This checks to see if the Delete method will throw
+        // an error when it tries to delate a record that has a
+        // Foreign key with another table.
         public void TestDeleteForeignKeyConstraint()
         {
             CustomerProps p = (CustomerProps)db.Retrieve(20);
@@ -58,6 +87,14 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This method is used to test the Update method from
+        // the CustomerDB class, where it first uses the Retrieve
+        // method to grab a already made object, they sets
+        // the Name field to something new, running the
+        // Update method with the edited record to update the
+        // record in the database. Running the Retrieve method
+        // again with the same record, using AreEqual to see if
+        // it's Name field was updated to what we wanted it to.
         public void TestUpdate()
         {
             CustomerProps p = (CustomerProps)db.Retrieve(1);
@@ -68,6 +105,13 @@ namespace MMABooksTests
         }
 
         [Test]
+        // This method is used to test the Create method from
+        // the CustomerDB class, where we populate the fields
+        // of a CustomerProps object which then we run it though
+        // the Create method to have it added to the Customer table
+        // of the database. Where using the Receive method again we
+        // grab the recently created record, using AreEqual to see
+        // if the one we made matches the one we just added.
         public void TestCreate()
         {
             CustomerProps p = new CustomerProps();
